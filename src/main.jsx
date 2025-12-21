51c68f2bdca75383d3d0edad99597cd907f384e9
MohammedAliBelay <eng.mame17@gmail.com>
2025-12-21 23:09:18 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { DataProvider } from "./components/dataProvider/DataProvider.jsx";
-import { reducer, initialState } from "./utility/reducer.js";
+import { reducer, initialState } from "./utility/Reducer.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
