a17fbc603cd61fa1bca887ed0b4f37aaa2488e68
MohammedAliBelay <eng.mame17@gmail.com>
2025-12-21 23:13:05 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import Routing from "./Routing.jsx";
-import { DataContext } from "./components/DataProvider/DataProvider.jsx";
+import { DataContext } from "./components/dataProvider/DataProvider.jsx";
 import { Type } from "./utility/action.type.js";
 import { auth } from "./Utility/firebase.js";
 
