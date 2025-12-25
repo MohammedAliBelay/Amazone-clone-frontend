5dd12906a4992a7dbdae612b92b985e71b28d5c9
MohammedAliBelay <eng.mame17@gmail.com>
2025-12-25 13:30:15 +0300

    final

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 //for Backend Communication
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/e-clone-dcba3/us-central1/api", // for local host
-  baseURL: "http://localhost:5000", // for local host
+  // baseURL: "http://localhost:5000", // for local host
 
-  // baseURL: "https://amazone-clone-backend-06tc.onrender.com", // for deployed backend on render.com
+  baseURL: "https://amazone-clone-backend-06tc.onrender.com", // for deployed backend on render.com
 });
 
 export { axiosInstance };
